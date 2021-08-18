f32ee592b42d4b630544c08746307eadb365418f
WullT <38081583+WullT@users.noreply.github.com>
2021-08-18 07:58:09 +0200

    Create get_paxcounter_battery.py

---
+++ TTNDownlink/get_paxcounter_battery.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import ssl
+import json
+
+command = """
+            {
+            "downlinks": [{
+                "f_port": 2,
+                "frm_payload": "gw==",
+                "priority": "HIGH",
+                "confirmed": false
+            }]
+            }
+"""
+
+f = open("/home/ubuntu/TTNDownlink/config.json", "r")
+config = json.loads(f.read())
+f.close()
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+
+client = mqtt.Client()
+client.username_pw_set(config["username"], config["password"])
+client.on_connect = on_connect
+
+
+client.connect(config["broker"], config["port"], 60)
+
+for device in config["devices"]:
+    client.publish(config["topicprefix"] + device + config["topicsuffix"], command, 2)
